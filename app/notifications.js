3af683d744c690305d34aed8303399bc37d7227b
Damon Sicore <damon@sicore.com>
2018-03-28 19:54:25 -0700

    Fixing the lintxings.

--- app/notifications.js
+++ app/notifications.js
@@ -3,28 +3,28 @@ const fetch = require('node-fetch');
 
 const {version} = require('./package');
 
- const NEWS_URL = 'https://pc-news.now.sh';
+const NEWS_URL = 'https://hyper-news.now.sh';
 
 module.exports = function fetchNotifications(win) {
   const {rpc} = win;
   const retry = err => {
     setTimeout(() => fetchNotifications(win), ms('30m'));
     if (err) {
-////eslint-disable-next-line no-console
-//      console.error('Notification messages fetch error', err.stack);
+      //eslint-disable-next-line no-console
+      console.error('Notification messages fetch error', err.stack);
     }
   };
-//eslint-disable-next-line no-console
-//  console.log('Checking for notification messages');
-//   fetch(NEWS_URL, {
- //   headers: {
-//      'X-pc-Version': version,
-//      'X-pc-Platform': process.platform
-  //  }
-//}//)
-//  .then(res => res.json())
-//   .then(data => {
-      const {message} = '';
+  //eslint-disable-next-line no-console
+  console.log('Checking for notification messages');
+  fetch(NEWS_URL, {
+    headers: {
+      'X-Hyper-Version': version,
+      'X-Hyper-Platform': process.platform
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      const {message} = data || {};
       if (typeof message !== 'object' && message !== '') {
         throw new Error('Bad response');
       }
