bd59e43af48765fe17fd7c65890c694c80ddff5d
Damon Sicore <damon@sicore.com>
2018-03-28 19:47:08 -0700

    Fixing lintssssss.

--- app/notifications.js
+++ app/notifications.js
@@ -1,41 +1,41 @@
-//const ms = require('ms');
-//const fetch = require('node-fetch');
+const ms = require('ms');
+const fetch = require('node-fetch');
 
-//const {version} = require('./package');
+const {version} = require('./package');
 
-// const NEWS_URL = 'https://pc-news.now.sh';
+ const NEWS_URL = 'https://pc-news.now.sh';
 
 module.exports = function fetchNotifications(win) {
-//  const {rpc} = win;
-//  const retry = err => {
-//    setTimeout(() => fetchNotifications(win), ms('30m'));
-//    if (err) {
-//      //eslint-disable-next-line no-console
+  const {rpc} = win;
+  const retry = err => {
+    setTimeout(() => fetchNotifications(win), ms('30m'));
+    if (err) {
+////eslint-disable-next-line no-console
 //      console.error('Notification messages fetch error', err.stack);
-//    }
-//  };
+    }
+  };
 //eslint-disable-next-line no-console
 //  console.log('Checking for notification messages');
-//  fetch(NEWS_URL, {
-//    headers: {
+//   fetch(NEWS_URL, {
+ //   headers: {
 //      'X-pc-Version': version,
 //      'X-pc-Platform': process.platform
-//    }
-//  })
-//    .then(res => res.json())
- //   .then(data => {
-//      const {message} = data || {};
-//      if (typeof message !== 'object' && message !== '') {
-//        throw new Error('Bad response');
-//      }
-//      if (message === '') {
-//        //eslint-disable-next-line no-console
-//        console.log('No matching notification messages');
-//      } else {
-//        rpc.emit('add notification', message);
-//      }
-//
-//      retry();
-//    })
-//    .catch(retry);
+  //  }
+//}//)
+//  .then(res => res.json())
+//   .then(data => {
+      const {message} = '';
+      if (typeof message !== 'object' && message !== '') {
+        throw new Error('Bad response');
+      }
+      if (message === '') {
+        //eslint-disable-next-line no-console
+        console.log('No matching notification messages');
+      } else {
+        rpc.emit('add notification', message);
+      }
+
+      retry();
+    })
+    .catch(retry);
 };
